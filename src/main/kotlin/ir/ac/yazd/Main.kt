c1419bc3236552bb0fb4ef4ed0cb95d58d3061d6
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-02-21 02:20:36 +0330

    Implement the logic to calculate edge counts

--- src/main/kotlin/ir/ac/yazd/Main.kt
+++ src/main/kotlin/ir/ac/yazd/Main.kt
@@ -3,8 +3,19 @@ package ir.ac.yazd
 import java.io.File
 
 fun main() {
-    File("src/main/resources/graph.txt").bufferedReader()
-        .lines()
-        .limit(10)
-        .forEach { println(it) }
+    val listOfEdgeCount = generateListOfEdgeCount()
+    val listOfMergedEdgeCount = mergeEdgeCounts(listOfEdgeCount)
+    listOfMergedEdgeCount.forEach { println("Edge count: ${it.key}, Frequency: ${it.value.size}") }
 }
+
+private fun generateListOfEdgeCount(): List<Int> {
+    return File("src/main/resources/graph.txt")
+        .bufferedReader()
+        .lineSequence()
+        // .take(200)
+        /* A map from node to its in-going edge count (substitute Before/After to switch between out- and in-degree) */
+        .groupBy({ it.substringAfter(" ").toInt() }, { it.substringBefore(" ").toInt() })
+        .map { it.value.size }
+}
+
+fun mergeEdgeCounts(list: List<Int>) = list.groupBy { it }/* Map from edge count to count */.entries.sortedBy { it.key }
