12bbcc6d9486be09c65faca1d209be14cd97c238
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-04-10 01:24:31 +0430

    Separate code specific to each project part into a separate file

---
+++ src/main/kotlin/ir/ac/yazd/AverageDistance.kt
@@ -0,0 +1,5 @@
+package ir.ac.yazd
+
+fun main() {
+
+}
--- src/main/kotlin/ir/ac/yazd/PowerLaw.kt
+++ src/main/kotlin/ir/ac/yazd/PowerLaw.kt
@@ -7,7 +7,6 @@ import org.thymeleaf.templateresolver.FileTemplateResolver
 import java.io.StringWriter
 import java.nio.file.Files
 import java.nio.file.Path
-import java.util.*
 import kotlin.math.log10
 import kotlin.math.pow
 
@@ -22,7 +21,6 @@ private val stringWriter = StringWriter()
 
 fun main() {
     determineIfAdheresPowerLaw1()
-    determineIfGraphIsBowTie()
 }
 
 private fun determineIfAdheresPowerLaw1() {
@@ -92,56 +90,4 @@ private fun setupTemplateEngine(thContext: Context): TemplateEngine {
     val templateEngine = TemplateEngine().apply { setTemplateResolver(templateResolver) }
     templateEngine.process(templateFilePath.toString(), thContext, stringWriter)
     return templateEngine
-}
-
-private fun determineIfGraphIsBowTie() {
-    val map = Files.newBufferedReader(sourceFilePath)
-        .lineSequence()
-        .take(100_000)
-        .groupBy({ it.substringBefore(" ").toInt() }, { it.substringAfter(" ").toInt() })
-
-
-    val visiting = mutableSetOf<Int>()
-    fun canFirstNodeReachTheSecondNode(first: Int, second: Int): Boolean {
-        if (!map.containsKey(first)) return false
-
-        if (map.getValue(first).contains(second)) return true
-
-        visiting.add(first)
-        var isConnected = false
-        for (n in map.getValue(first)) {
-            if (!visiting.contains(n)) {
-                isConnected = canFirstNodeReachTheSecondNode(n, second)
-                if (isConnected) break
-            }
-        }
-        return isConnected
-    }
-
-
-    val queue: Queue<Int> = ArrayDeque(map.keys)
-    val sets = mutableListOf(mutableSetOf(queue.element()))
-    var currentSet = 0
-
-    fun isNodeInSets(node: Int): Boolean {
-        for (set in sets) if (set.contains(node)) return true
-        return false
-    }
-
-    while (!queue.isEmpty()) {
-        val node = queue.remove()
-        visiting.clear()
-        if (!isNodeInSets(node) && !canFirstNodeReachTheSecondNode(sets[currentSet].first(), node)) {
-            currentSet++
-            sets.add(mutableSetOf(node))
-        }
-        for (target in map.getValue(node)) {
-            visiting.clear()
-            if (canFirstNodeReachTheSecondNode(target, node)) {
-                sets[currentSet].add(target)
-            }
-        }
-    }
-
-    sets.forEach { println(it) }
 }
