e867a197657e2bcd1ef689ee01f97cbac9702d56
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-04-10 03:06:13 +0430

    Fix the bug that caused error for vertices not having any edges

--- src/main/kotlin/ir/ac/yazd/AverageDistance.kt
+++ src/main/kotlin/ir/ac/yazd/AverageDistance.kt
@@ -10,7 +10,7 @@ fun main() {
             .lineSequence()
             .groupBy({ it.substringBefore(" ").toInt() }, { it.substringAfter(" ").toInt() })
 
-    val distance = calculateShortestDistance(12, 3)
+    val distance = calculateShortestDistance(15,14)
     println(distance)
 }
 
@@ -20,6 +20,7 @@ fun main() {
 private fun calculateShortestDistance(from: Int, to: Int): Int {
     val visited = mutableSetOf<Int>()
     fun calculate(from: Int, to: Int): Int {
+        if (!map.containsKey(from)) return -1
         if (from == to) return 0
         if (map[from]!!.contains(to)) return 1
 
