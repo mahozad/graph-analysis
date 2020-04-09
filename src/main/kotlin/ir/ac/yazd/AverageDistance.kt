4adfb4cef6636871bf3d8dd8a3746cf38dc10984
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-04-10 02:53:51 +0430

    Add initial impl. of calculating shortest distance between two nodes

--- src/main/kotlin/ir/ac/yazd/AverageDistance.kt
+++ src/main/kotlin/ir/ac/yazd/AverageDistance.kt
@@ -1,5 +1,39 @@
 package ir.ac.yazd
 
-fun main() {
+import java.nio.file.Files
+import java.nio.file.Path
 
+private val sourceFilePath: Path = Path.of("src/main/resources/sample-graph.txt")
+private lateinit var map: Map<Int, List<Int>>
+fun main() {
+    map = Files.newBufferedReader(sourceFilePath)
+            .lineSequence()
+            .groupBy({ it.substringBefore(" ").toInt() }, { it.substringAfter(" ").toInt() })
+
+    val distance = calculateShortestDistance(12, 3)
+    println(distance)
+}
+
+/**
+ * Uses BFS (Breadth-First-Search) algorithm.
+ */
+private fun calculateShortestDistance(from: Int, to: Int): Int {
+    val visited = mutableSetOf<Int>()
+    fun calculate(from: Int, to: Int): Int {
+        if (from == to) return 0
+        if (map[from]!!.contains(to)) return 1
+
+        visited.add(from)
+        val distances = mutableListOf<Int>()
+        for (node in map[from]!!) {
+            if (!visited.contains(node)) {
+                val distance = calculate(node, to)
+                if (distance != -1) distances.add(distance)
+            }
+        }
+        val distance = distances.min()
+        return if (distance == -1 || distance == null) -1 else 1 + distance
+    }
+
+    return calculate(from, to)
 }
