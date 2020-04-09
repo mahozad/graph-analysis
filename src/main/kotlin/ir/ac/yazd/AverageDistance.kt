c0517c298ec4f88c3ba293bcc54982b6177ea75c
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-04-10 03:23:37 +0430

    Refactor code

--- src/main/kotlin/ir/ac/yazd/AverageDistance.kt
+++ src/main/kotlin/ir/ac/yazd/AverageDistance.kt
@@ -10,7 +10,7 @@ fun main() {
             .lineSequence()
             .groupBy({ it.substringBefore(" ").toInt() }, { it.substringAfter(" ").toInt() })
 
-    val distance = calculateShortestDistance(15,14)
+    val distance = calculateShortestDistance(1, 4)
     println(distance)
 }
 
@@ -20,20 +20,20 @@ fun main() {
 private fun calculateShortestDistance(from: Int, to: Int): Int {
     val visited = mutableSetOf<Int>()
     fun calculate(from: Int, to: Int): Int {
-        if (!map.containsKey(from)) return -1
         if (from == to) return 0
-        if (map[from]!!.contains(to)) return 1
+        if (!map.containsKey(from)) return -1
+        if (map[from]!!.contains(to)) return +1
 
         visited.add(from)
         val distances = mutableListOf<Int>()
         for (node in map[from]!!) {
             if (!visited.contains(node)) {
                 val distance = calculate(node, to)
-                if (distance != -1) distances.add(distance)
+                if (distance != -1) distances.add(distance + 1)
             }
         }
-        val distance = distances.min()
-        return if (distance == -1 || distance == null) -1 else 1 + distance
+
+        return distances.min() ?: -1
     }
 
     return calculate(from, to)
