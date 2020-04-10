03f1532e1c1874f2d5355280e68c1ecaf9fff008
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-04-11 01:46:52 +0430

    Use the whole graph instead of reading from file to increase performance; Make calculation multi-threaded; Add some improvements

--- src/main/kotlin/ir/ac/yazd/AverageDistance.kt
+++ src/main/kotlin/ir/ac/yazd/AverageDistance.kt
@@ -2,23 +2,37 @@ package ir.ac.yazd
 
 import java.nio.file.Files
 import java.nio.file.Path
+import java.util.Collections.synchronizedList
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
-private val sourceFilePath = Path.of("src/main/resources/sample-graph.txt")
-// private val graph = Files.newBufferedReader(sourceFilePath).lineSequence().groupBy({ it.substringBefore(" ").toInt() }, { it.substringAfter(" ").toInt() })
-private val graphNodes = Files.newBufferedReader(sourceFilePath).lineSequence().map { it.substringBefore(" ").toInt() }.toSet()
+private val sourceFilePath = Path.of("src/main/resources/graph.txt")
+private val graph = Files.newBufferedReader(sourceFilePath)
+    .lineSequence()
+    .groupBy(
+        { it.substringBefore(" ").toInt() },
+        { it.substringAfter(" ").toInt() }
+    )
+
+//private val graphNodes = Files.newBufferedReader(sourceFilePath).lineSequence().map { it.substringBefore(" ").toInt() }.toSet()
+private val distances = synchronizedList(ArrayList<Int>())
 
 fun main() {
-    val nodes = mutableSetOf<Int>()
-    for (i in 0..1000) nodes.add(graphNodes.random())
+    val executorService = Executors.newFixedThreadPool(4)
+
+    val nodes = getRandomNodes()
 
     for (i in 0..nodes.size) {
-        for (j in i + 1..nodes.size) {
+        for (j in i + 1 until nodes.size) {
             val node1 = nodes.elementAt(i)
             val node2 = nodes.elementAt(j)
-            println(calculateShortestDistance(node1, node2))
-            println(calculateShortestDistance(node2, node1))
+            executorService.submit(DistanceCalculator(node1, node2))
+            executorService.submit(DistanceCalculator(node2, node1))
         }
     }
+
+    executorService.awaitTermination(30, TimeUnit.DAYS)
+    println((distances.reduce { total, distance -> total + distance } / distances.size))
 }
 
 /**
@@ -28,12 +42,12 @@ private fun calculateShortestDistance(from: Int, to: Int): Int {
     val visited = mutableSetOf<Int>()
     fun calculate(from: Int, to: Int): Int {
         if (from == to) return 0
-        if (!graphNodes.contains(from)) return -1 // Required
-        if (edgesOf(from).contains(to)) return +1
+        if (!graph.keys.contains(from)) return -1 // Required
+        if (graph.getValue(from).contains(to)) return +1
 
         visited.add(from)
         val distances = mutableListOf<Int>()
-        for (neighbor in edgesOf(from)) {
+        for (neighbor in graph.getValue(from)) {
             if (visited.contains(neighbor)) continue
             val neighborDistance = calculate(neighbor, to)
             if (neighborDistance != -1) distances.add(1 + neighborDistance)
@@ -45,11 +59,25 @@ private fun calculateShortestDistance(from: Int, to: Int): Int {
     return calculate(from, to)
 }
 
+private fun getRandomNodes(): Set<Int> {
+    val nodes = mutableSetOf<Int>()
+    for (i in 0..1000) nodes.add(graph.keys.random())
+    return nodes
+}
+
+class DistanceCalculator(private val node1: Int, private val node2: Int) : Runnable {
+    override fun run() {
+        val distance = calculateShortestDistance(node1, node2)
+        if (distance > 0) distances.add(distance)
+        println(distances.size)
+    }
+}
+
 private fun edgesOf(node: Int): Collection<Int> {
     return Files.newBufferedReader(Path.of("src/main/resources/edges/out/${node % 1000}.txt"))
-            .lineSequence()
-            .first { it.startsWith("$node->") }
-            .substringAfter("->")
-            .split(" ")
-            .map { it.toInt() }
+        .lineSequence()
+        .first { it.startsWith("$node->") }
+        .substringAfter("->")
+        .split(" ")
+        .map { it.toInt() }
 }
