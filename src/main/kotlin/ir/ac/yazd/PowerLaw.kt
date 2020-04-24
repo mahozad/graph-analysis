ab05c851c8eab0227ade1082c3ae88b52cc7281d
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-04-24 21:27:57 +0430

    Remove determineIfAdheresPowerLaw2() method and reformat code

--- src/main/kotlin/ir/ac/yazd/PowerLaw.kt
+++ src/main/kotlin/ir/ac/yazd/PowerLaw.kt
@@ -10,7 +10,6 @@ import java.nio.file.Path
 import java.time.Duration
 import java.time.Instant
 import kotlin.math.log10
-import kotlin.math.pow
 
 private val sourceFilePath = Path.of("src/main/resources/graph.txt")
 private val outputFilePath = Path.of("result.html")
@@ -22,10 +21,6 @@ private val stringWriter = StringWriter()
 // stack size to 1 Megabyte
 
 fun main() {
-    determineIfAdheresPowerLaw1()
-}
-
-private fun determineIfAdheresPowerLaw1() {
     val startTime = Instant.now()
 
     val edgeCounts = generateListOfIngoingEdgeCount()
@@ -45,50 +40,18 @@ private fun determineIfAdheresPowerLaw1() {
     Files.newBufferedWriter(outputFilePath).use { writer -> writer.write(stringWriter.toString()) }
 }
 
-private fun determineIfAdheresPowerLaw2() {
-    val edgeCounts = generateListOfIngoingEdgeCount()
-    val edgeCountFreq = mergeEdgeCounts(edgeCounts)
-
-    val thContext = Context()
-    thContext.setVariable("edgeCounts", edgeCountFreq.map { it.first })
-    thContext.setVariable("edgeCountFreq", edgeCountFreq.map { it.second })
-    // groupByPowerOf10(edgeCountFreq, thContext)
-
-    setupTemplateEngine(thContext)
-
-    Files.newBufferedWriter(outputFilePath).use { writer -> writer.write(stringWriter.toString()) }
-
-    fun groupByPowerOf10(edgeCountToFreq: List<Pair<Int, Int>>, thContext: Context) {
-        val freq = mutableMapOf<String, Int>()
-        try {
-            for (i in 0..10) {
-                val from = 10.0.pow(i).toInt() - 1
-                val to = 10.0.pow(i + 1).toInt() - 2
-                for (j in from until to) {
-                    freq.merge("10^${i}-10^${i + 1}", edgeCountToFreq[j].second) { t, u -> t + u }
-                }
-            }
-        } catch (e: Exception) {
-
-        }
-        thContext.setVariable("edgeCounts", freq.keys)
-        thContext.setVariable("edgeCountFreq", freq.values)
-    }
-}
-
 private fun generateListOfIngoingEdgeCount(): List<Int> {
     return Files.newBufferedReader(sourceFilePath)
-            .lineSequence()
-            // .take(1000)
-            // Map node to its in-going edge count (substitute Before/After to switch between out and in)
-            .groupBy({ it.substringAfter(" ").toInt() }, { it.substringBefore(" ").toInt() })
-            .map { it.value.size }
+        .lineSequence()
+        // Map node to its in-going edge count (substitute Before/After to switch between out and in)
+        .groupBy({ it.substringAfter(" ").toInt() }, { it.substringBefore(" ").toInt() })
+        .map { it.value.size }
 }
 
 private fun mergeEdgeCounts(list: List<Int>) = list
-        .groupBy { it }
-        .entries.sortedBy { it.key }
-        .map { Pair(it.key, it.value.size) }
+    .groupBy { it }
+    .entries.sortedBy { it.key }
+    .map { Pair(it.key, it.value.size) }
 
 private fun filterOnlyPowersOf10(edgeCountFreq: List<Pair<Int, Int>>) = edgeCountFreq.filter {
     it.first == 1 || it.first == 10 || it.first == 100 || it.first == 1000 || it.first == 10000 || it.first == 100000
