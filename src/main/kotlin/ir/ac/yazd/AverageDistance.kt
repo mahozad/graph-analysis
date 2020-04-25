0b4e4b25a27ed9a5d18a08f18d7a06a6dcbeb680
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-04-25 13:40:48 +0430

    Remove allDistancesFoundSoFar field because it had no benefit

--- src/main/kotlin/ir/ac/yazd/AverageDistance.kt
+++ src/main/kotlin/ir/ac/yazd/AverageDistance.kt
@@ -6,19 +6,15 @@ import java.time.Duration
 import java.time.Instant
 import java.util.*
 import java.util.Collections.synchronizedList
-import java.util.Collections.synchronizedMap
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit.DAYS
 import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 private val sourceFilePath = Path.of("src/main/resources/graph.txt")
 private val graph = Files.newBufferedReader(sourceFilePath)
     .lineSequence()
     .groupBy({ it.substringBefore(" ").toInt() }, { it.substringAfter(" ").toInt() })
 
-//private val graphNodes = Files.newBufferedReader(sourceFilePath).lineSequence().map { it.substringBefore(" ").toInt() }.toSet()
-private val allDistancesFoundSoFar = synchronizedMap(HashMap<Pair<Int, Int>, Int>())
 private val targetNodesDistances = synchronizedList(ArrayList<Int>())
 
 fun main() {
@@ -38,17 +34,14 @@ fun main() {
     println("Time: ${Duration.between(startTime, Instant.now()).toMinutes()}m")
 }
 
-/**
- * Uses BFS (Breadth-First-Search) algorithm.
- */
+// Uses BFS (Breadth-First-Search) algorithm.
 private fun calculateShortestDistance(from: Int, to: Int): Int {
     if (from == to) return 0
-    if (graph.getValue(from).contains(to)) return +1
-    if (allDistancesFoundSoFar.keys.contains(Pair(from, to))) return allDistancesFoundSoFar[Pair(from, to)]!!
+    if (graph.getValue(from).contains(to)) return 1 // Shortcut
 
     val queue: Queue<Int> = LinkedList()
     queue.add(from)
-    queue.add(null)
+    queue.add(null) // Indicates end of this level
     var depth = 1
     val visited = mutableSetOf<Int>()
 
@@ -70,6 +63,7 @@ private fun calculateShortestDistance(from: Int, to: Int): Int {
         queue.addAll(graph.getValue(next))
         if (queue.element() == null) queue.add(null) // All nodes of this level finished so add flag for next level
     }
+
     return -1
 }
 
