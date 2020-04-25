5ad0a2e650955863f7c9cb80f79f5e6e806a7042
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-04-25 18:21:10 +0430

    Upgrade the algorithm to use the method suggested in a paper

--- src/main/kotlin/ir/ac/yazd/BowTie.kt
+++ src/main/kotlin/ir/ac/yazd/BowTie.kt
@@ -2,76 +2,63 @@ package ir.ac.yazd
 
 import java.nio.file.Files
 import java.nio.file.Path
+import java.time.Duration
+import java.time.Instant
 import java.util.*
 
-private val sourceFilePath = Path.of("src/main/resources/graph.txt")
+private val sourceFilePath = Path.of("src/main/resources/sample-graph.txt")
 private lateinit var graph: Map<Int, List<Int>>
+private lateinit var graphConverse: Map<Int, List<Int>>
 
 // -Xmx4096m -Xss128m
 fun main() {
+    readGraph()
     determineIfGraphIsBowTie()
 }
 
 private fun determineIfGraphIsBowTie() {
-    readGraph()
+    val startTime = Instant.now()
 
     val queue: Queue<Int> = ArrayDeque(graph.keys)
-    val sets = mutableListOf(mutableSetOf<Int>())
-    val visited = mutableSetOf<Int>()
-    var currentSet = 0
     while (queue.isNotEmpty()) {
         val node = queue.remove()
-        if (visited.contains(node)) continue
-        sets[currentSet].add(node)
-        visited.add(node)
-        if (!graph.containsKey(node)) continue // If has no outgoing ...
-        val thisClusterQueue = ArrayDeque(graph.neighborsOf(node))
+        val nodeReachables = findNodesReachableToOrFrom(graph, node)
+        val nodeReachings = findNodesReachableToOrFrom(graphConverse, node)
 
-        while (thisClusterQueue.isNotEmpty()) {
-            val neighbor = thisClusterQueue.remove()
-            if (visited.contains(neighbor)) continue
-            if (canFirstNodeReachTheSecondNode(neighbor, node)) {
-                sets[currentSet].add(neighbor)
-                visited.add(neighbor)
-                queue.remove(neighbor)
-                thisClusterQueue.addAll(graph.neighborsOf(neighbor))
-            }
-        }
-
-        sets.add(mutableSetOf())
-        currentSet++
+        val ssc = nodeReachables.intersect(nodeReachings).union(setOf(node))
+        queue.removeAll(ssc)
     }
 
-    sets.forEach { println(it) }
+    println("Time: ${Duration.between(startTime, Instant.now()).toMinutes()}m")
+}
+
+
+fun findNodesReachableToOrFrom(graph:Map<Int, List<Int>>, node: Int): Set<Int> {
+    val result = mutableSetOf<Int>()
+    val visited = mutableSetOf<Int>()
+
+    fun run(node: Int): Set<Int> {
+        if (!graph.containsKey(node)) return result
+        visited.add(node)
+        for (neighbor in graph.neighborsOf(node)) {
+            if (!visited.contains(neighbor)) {
+                result.add(neighbor)
+                run(neighbor)
+            }
+        }
+        return result
+    }
+
+    return run(node)
 }
 
 fun readGraph() {
     graph = Files.newBufferedReader(sourceFilePath)
         .lineSequence()
         .groupBy({ it.substringBefore(" ").toInt() }, { it.substringAfter(" ").toInt() })
+    graphConverse = Files.newBufferedReader(sourceFilePath)
+        .lineSequence()
+        .groupBy({ it.substringAfter(" ").toInt() }, { it.substringBefore(" ").toInt() })
 }
 
 private fun Map<Int, List<Int>>.neighborsOf(node: Int) = getValue(node)
-
-fun canFirstNodeReachTheSecondNode(first: Int, second: Int): Boolean {
-    val visited = mutableSetOf<Int>()
-
-    fun run(first: Int, second: Int): Boolean {
-        if (!graph.containsKey(first)) return false
-        if (graph.neighborsOf(first).contains(second)) return true
-
-        visited.add(first)
-        var isConnected = false
-        for (neighbor in graph.neighborsOf(first)) {
-            if (!visited.contains(neighbor)) {
-                isConnected = run(neighbor, second)
-                if (isConnected) break
-            }
-        }
-
-        return isConnected
-    }
-
-    visited.clear()
-    return run(first, second)
-}
